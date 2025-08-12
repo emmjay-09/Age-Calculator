b7935e48a5ff18d5b6aa2a0be4e094cc10861855
Mujib <mujibidrees@gmail.com>
2025-08-12 21:13:39 +0100

    Initial commit

---
+++ age calculator.js
@@ -0,0 +1,104 @@
+document.getElementById("button").addEventListener("click", displayDate);
+
+const current = new Date();
+
+const currentMonth = current.getMonth();
+
+const currentYear = current.getFullYear();
+
+const messageBox = document.querySelector(".message");
+
+const borderLine = document.querySelector(".bar");
+
+function displayDate() {
+  const dateSelect = document.getElementById("bar");
+
+  const dateValue = dateSelect.value;
+
+  const dateObject = new Date(dateValue);
+
+  let getMonth = dateObject.getMonth();
+
+  const emptyMonth = isNaN(getMonth);
+
+  const monthDiff = currentMonth - getMonth;
+
+  const getYear = dateObject.getFullYear();
+
+  const ageValue = currentYear - getYear;
+
+  if (currentYear < getYear && getMonth === currentMonth) {
+    messageDisplay("message2");
+    errorMessage();
+    setTimeout(removeError, 1000);
+  }
+  if (getYear === currentYear && getMonth > currentMonth) {
+    messageDisplay("message2");
+    errorMessage();
+    setTimeout(removeError, 1000);
+  } else if (currentMonth < getMonth) {
+    messageDisplay("message1");
+  } else if (getYear === currentYear && getMonth < currentMonth) {
+    messageDisplay("message5");
+  } else if (getMonth < currentMonth) {
+    messageDisplay("message4");
+  } else if (getMonth === currentMonth && getYear === currentYear) {
+    messageDisplay("message2");
+    errorMessage();
+    setTimeout(removeError, 1000);
+  } else {
+    messageDisplay("message6");
+    errorMessage();
+    setTimeout(removeError, 1000);
+  }
+
+  function messageDisplay(key) {
+    if (key === "message1") {
+      messageBox.textContent =
+        "You are " +
+        (ageValue - 1) +
+        " year(s) and " +
+        (monthDiff + 12) +
+        " month(s) old.";
+    } else if (key === "message2") {
+      messageBox.textContent = "Invalid date";
+    } else if (key === "message3") {
+      messageBox.textContent = "You are a year old.";
+    } else if (key === "message4") {
+      messageBox.textContent =
+        "You are " + ageValue + " year(s) and " + monthDiff + " month(s) old.";
+    } else if (key === "message5") {
+      messageBox.textContent = "You are " + monthDiff + " month(s) old.";
+    } else if (key === "message6") {
+      messageBox.textContent = "Please select a date";
+    }
+  }
+}
+
+function checkValue(emptyMonth) {
+  const test = document.getElementById("clear");
+
+  const testing = test.addEventListener("click", () => {
+    if (emptyMonth) {
+      console.log("hi");
+    }
+  });
+}
+
+function errorMessage() {
+  if (messageBox) {
+    messageBox.classList.add("error");
+  }
+  if (borderLine) {
+    borderLine.classList.add("error");
+  }
+}
+
+function removeError() {
+  if (messageBox) {
+    messageBox.classList.remove("error");
+  }
+  if (borderLine) {
+    borderLine.classList.remove("error");
+  }
+}
